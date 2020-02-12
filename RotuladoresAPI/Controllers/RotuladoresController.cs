c10035be06f37e672764c8d5dddb7c88d4dfaced
Sergio Velasco <sergio.velasco@bravent.net>
2020-02-12 12:26:50 +0100

    API layer created

---
+++ RotuladoresAPI/Controllers/RotuladoresController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using RotuladoresDomain.Services;
+using RotuladoresDomain.Models;
+using AutoMapper;
+using RotuladoresDTO;
+
+namespace EjemploAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RotuladoresController : ControllerBase
+    {
+        private readonly IRotuladorService _rotuladorService;
+
+        public RotuladoresController(IRotuladorService rotuladorService)
+        {
+            _rotuladorService = rotuladorService;
+        }
+
+        // GET: api/Rotuladores
+        [HttpGet]
+        public ActionResult<IEnumerable<RotuladorDTO>> Read()
+        {
+           return _rotuladorService.Get();
+        }
+
+        // GET: api/Rotuladores/5
+        [HttpGet("{id:length(24)}", Name = "ReadRotulador")]
+        public ActionResult<RotuladorDTO> Read(string id)
+        {
+            var rotulador = _rotuladorService.Get(id);
+
+            if (rotulador == null)
+            {
+                return NotFound();
+            }
+
+            return rotulador;
+        }
+
+        // POST: api/Rotuladores
+        [HttpPost]
+        public ActionResult<RotuladorDTO> Create(RotuladorDTO rotulador)
+        {
+            _rotuladorService.Create(rotulador);
+
+            return CreatedAtRoute("ReadRotulador", new { id = rotulador.Id.ToString() }, rotulador);
+        }
+
+        // PUT: api/Rotuladores/5
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, RotuladorDTO rotuladorIn)
+        {
+            var rotulador = _rotuladorService.Get(id);
+
+            if (rotulador == null)
+            {
+                return NotFound();
+            }
+
+            _rotuladorService.Update(id, rotuladorIn);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Rotuladores/5
+        [HttpDelete("{id:length(24)}")]
+        public ActionResult<RotuladorDTO> DeleteRotulador(string id)
+        {
+            var rotulador = _rotuladorService.Get(id);
+
+            if (rotulador == null)
+            {
+                return NotFound();
+            }
+
+            _rotuladorService.Remove(rotulador);
+
+            return NoContent();
+        }
+    }
+}
---
+++ RotuladoresAPI/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using AutoMapper;
+using RotuladoresDomain.Mappers;
+using RotuladoresDomain.Repositories;
+using RotuladoresDomain.Services;
+using RotuladoresInfrastructure.Repositories;
+using RotuladoresInfrastructure.Context;
+using RotuladoresInfrastructure.Mongo;
+
+namespace RotuladoresAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<MongoDatabaseSettings>(
+                Configuration.GetSection(nameof(MongoDatabaseSettings)));
+
+            services.AddSingleton<IMongoDatabaseSettings>(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<MongoDatabaseSettings>>().Value);
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new RotuladorMapper());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddSingleton<IRotuladorContext, RotuladorContext>();
+            services.AddSingleton<IRotuladorRepository, RotuladorRepository>();
+            services.AddSingleton<IRotuladorService, RotuladorService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

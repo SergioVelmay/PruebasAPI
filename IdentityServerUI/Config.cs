244c0aa9d1533842a035a031858320411bf5c0dd
SergioVelmay <sergiovelmay@gmail.com>
2020-02-13 12:29:12 +0100

    UI Identity Server project created

---
+++ IdentityServerUI/Config.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServerUI
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[]
+            {
+                new ApiResource("rotuladores", "API Rotuladores")
+            };
+
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                // client credentials flow client
+                new Client
+                {
+                    ClientId = "sergio",
+                    ClientName = "Sergio Credentials Client",
+
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = { new Secret("prueba_123".Sha256()) },
+
+                    AllowedScopes = { "rotuladores" }
+                },
+
+                // MVC client using code flow + pkce
+                new Client
+                {
+                    ClientId = "sergioMVC",
+                    ClientName = "Sergio MVC Client",
+
+                    AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                    RequirePkce = true,
+                    ClientSecrets = { new Secret("prueba_MVC".Sha256()) },
+
+                    RedirectUris = { "http://localhost:5002/signin-oidc" },
+                    FrontChannelLogoutUri = "http://localhost:5002/signout-oidc",
+                    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+                    AllowOfflineAccess = true,
+                    AllowedScopes = { "openid", "profile", "rotuladores" }
+                },
+
+                // SPA client using code flow + pkce
+                new Client
+                {
+                    ClientId = "sergioSPA",
+                    ClientName = "Sergio SPA Client",
+                    ClientUri = "http://identityserver.io",
+
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RequirePkce = true,
+                    RequireClientSecret = false,
+
+                    RedirectUris =
+                    {
+                        "http://localhost:5002/index.html",
+                        "http://localhost:5002/callback.html",
+                        "http://localhost:5002/silent.html",
+                        "http://localhost:5002/popup.html",
+                    },
+
+                    PostLogoutRedirectUris = { "http://localhost:5002/index.html" },
+                    AllowedCorsOrigins = { "http://localhost:5002" },
+
+                    AllowedScopes = { "openid", "profile", "rotuladores" }
+                }
+            };
+    }
+}
